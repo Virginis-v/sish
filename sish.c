53bb24208f178e45b398ef327f4480e0dd20acf6
Virginis-v <152088611+Virginis-v@users.noreply.github.com>
2023-12-03 18:52:28 +0300

    Update sish.c

--- sish.c
+++ sish.c
@@ -7,5 +7,9 @@ return 0; }
 
 
 void begin() {
-  
-}
+  char* cwdbuf[1024];
+  if (getcwd(cwdbuf ,cwdbuf[1024]-1) == *cwdbuf) {// successfully accessed workin dir
+    // do nothing, this is uh
+  } else { perror("Fuck, getcwd failed"); }
+  }
+
