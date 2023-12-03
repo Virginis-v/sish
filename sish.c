f1952f98ef282fc842a2a8cb6eaa3d6671203752
Virginis-v <152088611+Virginis-v@users.noreply.github.com>
2023-12-03 18:43:24 +0300

    Initial commit 4

--- sish.c
+++ sish.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+#TODO: Move all definals to another file
 int main(int argc, char* argv[]) {
   if (argc == 1) { begin() } else if (argc == 2) {
   
